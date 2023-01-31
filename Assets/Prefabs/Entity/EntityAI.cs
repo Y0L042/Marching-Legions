925efadaa2be7db89fa251d298024fb7f3025442
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2023-01-31 16:30:30 +0200

    EndOfDay Save
    WIP - NPC Ai

--- Assets/Prefabs/Entity/EntityAI.cs
+++ Assets/Prefabs/Entity/EntityAI.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class EntityAI : MonoBehaviour, IEntityIntelligence
 {
-	public Vector3 MoveDirectionOutput 
-    { 
-        get;
-        set; 
-    }
+    Vector3 _movementDirection;
+
 
     [SerializeField][Tooltip("Adds the functionality to move the entity.")]
     EntityMovementComponent _movementComponent;
 
+    void FixedUpdate()
+    {
+        if (_movementComponent != null) _movementComponent.MoveBody(_movementDirection);
+    }
 
+    Vector3 RandomWander()
+    {
+
+        return Vector3.zero;
+    }
+
+
+    // void Raycast()
+    // {
+    //     Ray ray = new Ray(transform.position, transform.forward);
+    //     RaycastHit hit;
+    //     if (Physics.Raycast(ray, out hit, raycastLength))
+    //     {
+    //         Vector3 avoidDirection = (transform.position - hit.point).normalized;
+    //         transform.position += avoidDirection * avoidForce * Time.fixedDeltaTime;
+    //     }
+    // }
     
 }
--- Assets/Prefabs/Interfaces/IEntityIntelligence.cs
+++ Assets/Prefabs/Interfaces/IEntityIntelligence.cs
@@ -4,6 +4,5 @@ using UnityEngine;
 
 public interface IEntityIntelligence
 {
-    Vector3 MoveDirectionOutput{ get; set;}
 
 }
