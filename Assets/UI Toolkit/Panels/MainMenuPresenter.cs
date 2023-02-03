b6606791766c8eaa8d68cd0539717e9cb30be833
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2023-02-03 14:59:18 +0200

    EndOfDay Save
    Added a main menu UI and scene.
    - menu needs to be wired up
    - scene changing/starging needs to be implemented
    Scenes to be added:
    - settings
    - level picker
    - pause menu
    - game over
    Add the ability to save settings

--- Assets/UI Toolkit/Panels/MainMenuPresenter.cs
+++ Assets/UI Toolkit/Panels/MainMenuPresenter.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MainMenuPresenter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        root.Q<Button>("Start").clicked += () => Debug.Log("Start");
+        root.Q<Button>("Settings").clicked += () => Debug.Log("Settings");
+        root.Q<Button>("Quit").clicked += () => Debug.Log("Quit");
     }
 }
